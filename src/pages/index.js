6b88a5f834bc9db4437c2ed24379a87a71f74c08
rodrigueness7 <alex.correarodrigues@hotmail.com>
2024-02-19 17:53:42 -0300

    add sum of values

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 function Home() {
 
   const [data, setData] = useState([])
- 
+  const [sum, setSum] = useState([])
 
   useEffect(() =>{
      fetch('http://localhost:3001/')
@@ -14,12 +14,22 @@ function Home() {
     })
   }, [])
 
+  useEffect(() => {
+    fetch('http://localhost:3001/sum')
+    .then((res) => res.json())
+    .then((total) => {
+      setSum(total)
+    })
+  }, [])
 
   return(
     <div className="card">
       <h1>Despesas</h1>
       <div className="title"><span>Conta</span><span>Valor</span><span>Vencimento</span></div>
       {data.map((itens) => <div className="itens"><span className="description"> {itens.description}</span><span className="value">R$: {itens.value}</span><span> {new Date(itens.dt_exp).toLocaleDateString('pt-BR')}</span></div> )}
+      <div>
+        {sum.map(sum => <p>Total: R$: {sum['SUM(value)']}</p> )}
+      </div>
     </div>
   )
 
