02935edb374b0cff43fe059990822c0abe414d4a
rodrigueness7 <alex.correarodrigues@hotmail.com>
2024-02-27 16:49:16 -0300

    correcting the data update route

--- src/pages/update/[id].js
+++ src/pages/update/[id].js
@@ -1,35 +1,50 @@
-import React, { useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
+var initialDescription
+var initialValue
+var initialDate
+
+fetch('http://localhost:3001/find/59')
+    .then((res) => res.json())
+    .then((data) => {
+        initialDescription = data[0].description
+        initialValue = data[0].value
+        initialDate = new Date(data[0].dt_exp).toLocaleDateString('pt-br').split('/').reverse().join( '-' )
+    })
 
 const FormUpdate = () => {
-    let  initialDescription = useRef();
-    let initialValue = useRef();
-    let initialDt_exp = useRef();
+   
+    const [description, setDescription] = useState(initialDescription)
+    const [value, setValue] = useState(initialValue)
+    const [date, setDate] = useState(initialDate)
+   
+    const handleDescription = (e) => {
+        setDescription(e.target.value)
+    }
+
+    const handleValue = (e) => {
+        setValue(e.target.value)
+    }
+
+    const hanldeDate = (e) => {
+        setDate(e.target.value)
+        console.log(e.target.value)
+    }
     
-
-    useEffect(() => {
-        fetch('http://localhost:3001/find/34')
-        .then((res) => res.json())
-        .then((data) => {
-            initialDescription.current = data[0].description
-            initialValue.current = data[0].value
-            initialDt_exp.current = data[0].dt_exp
-            
-        })
-    },[])
-
-
-    return(
+    
+    return (
         <div>
-            <h2>Edite o item</h2>
+            <h2>Atualizar dados</h2>
             <form>
-                <input type="text" placeholder={initialDescription.current}></input>
-                <input type="number" placeholder={initialValue.current}></input>
-                <input type="date" placeholder={initialDt_exp.current}></input>
+                <input type="text" onChange={handleDescription} value={description} placeholder="Description"></input>
+                <input type="number" onChange={handleValue} value={value} placeholder="Value"></input>
+                <input type="date" onChange={hanldeDate} value={date}></input>
+                <button type="submit">Atualizar</button>
             </form>
         </div>
     )
-}
 
+}
 
 export default FormUpdate
